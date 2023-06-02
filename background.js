9b2fb170e0cf5abf848c06c1fb79ced0ab8c560b
verdaan <oyevardaan@gmail.com>
2023-06-02 00:39:19 -0700

    add check for existing pinned tabs, before opening a new set

--- background.js
+++ background.js
@@ -3,6 +3,13 @@ import { openPinnedTabs } from './utils.js';
 // event listener for when a new window is created
 chrome.windows.onCreated.addListener((window) => {
 	const newWindowId = window.id;
-	// open pinned tabs
-	openPinnedTabs(newWindowId);
+	// check existing pinned tabs
+	chrome.tabs.query({ pinned: true }).then((openedPinnedTabs) => {
+		// close existing pinned tabs, if any
+		const openedPinnedTabIds = openedPinnedTabs.map((tab) => tab.id);
+		chrome.tabs.remove(openedPinnedTabIds).then(() => {
+			// open fresh pinned tabs
+			openPinnedTabs(newWindowId);
+		});
+	});
 });
