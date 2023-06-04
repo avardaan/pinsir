df47a00313851a67c86d446146a078e71998911c
Vardaan Aashish <26334821+avardaan@users.noreply.github.com>
2023-06-03 17:42:51 -0700

    v1.01 (#8)

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 import { openPinnedTabs } from './utils.js';
 
-// event listener for when a new window is created
-chrome.windows.onCreated.addListener((window) => {
+function onNewWindowCreated(window) {
 	const newWindowId = window.id;
-	// open pinned tabs
-	openPinnedTabs(newWindowId);
+	// check existing pinned tabs
+	chrome.tabs.query({ pinned: true }).then((openedPinnedTabs) => {
+		// close existing pinned tabs, if any
+		const openedPinnedTabIds = openedPinnedTabs.map((tab) => tab.id);
+		chrome.tabs.remove(openedPinnedTabIds).then(() => {
+			// open fresh pinned tabs
+			openPinnedTabs(newWindowId);
+		});
+	});
+}
+
+// event listener for when a new window is created
+chrome.windows.onCreated.addListener(onNewWindowCreated, {
+	windowTypes: ['normal'],
 });
